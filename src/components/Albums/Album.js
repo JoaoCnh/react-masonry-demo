22344f7b776da597460d43d8946071c08dcf78d9
João Cunha <joao.cunha@wiremaze.com>
2017-07-27 14:12:09 +0000

    Updates

--- src/components/Albums/Album.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default ({ album }) => (
-	<a href={album.lastfm_url} className="album" target="_blank">
-		<img src={album.image} alt={album.name} />
-	</a>
-);
